547ad88067f5bda533a0caa89cde462419c18363
Sijey <Popov040391@gmail.com>
2019-10-18 14:00:29 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('hello mate academy!');
+document.querySelector('#thumbs').onclick = (event1) => {
+  const thumbnail = event1.target.closest('a');
+
+  if (!thumbnail) {
+    return;
+  }
+  showThumb(thumbnail.href, thumbnail.title);
+  event1.preventDefault();
+};
+
+function showThumb(href, title) {
+  document.querySelector('#largeImg').src = href;
+  document.querySelector('#largeImg').alt = title;
+}
