5204b23a51fa1466a6eccd2c45b802b147d7ee7e
Andrii <farikpite@gmail.com>
2019-10-18 21:10:16 +0300

    How I spend my summer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('hello mate academy!');
+const largeImg = document.querySelector('.gallery__large-img');
+const list = document.querySelector('#thumbs');
+
+list.addEventListener('click', (action) => {
+  action.preventDefault();
+  const clicked = action.target.closest('.list-item__link');
+  chenging(clicked.href, clicked.title);
+});
+
+function chenging(link, title) {
+  largeImg.src = link;
+  largeImg.alt = title;
+}
