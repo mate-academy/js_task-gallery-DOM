298dd27562643e6489b1914fa6d7dbb444da6ccf
Anna Kazantseva <annetastara@gmail.com>
2019-10-18 20:17:12 +0300

    Gallery in apple shop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
-
-console.log('hello mate academy!');
+const mainContainer = document.getElementById('thumbs');
+// eslint-disable-next-line no-shadow
+mainContainer.addEventListener('click', event => {
+  if (!event.target.closest('a')) {
+    return;
+  }
+  event.preventDefault();
+  const largeImage = document.getElementById('largeImg');
+  largeImage.src = event.target.closest('a').href;
+});
