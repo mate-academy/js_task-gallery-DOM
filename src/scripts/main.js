bfe5dd4618f55de793fdc1f0c5a9634e1c745c39
Rudenko Artem <artem.rud@bk.ru>
2019-10-20 14:36:43 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('hello mate academy!');
+const largeImg = document.getElementById('largeImg');
+const gallery = document.getElementById('thumbs');
+
+gallery.addEventListener('click', function(e) {
+  if (e.target.classList.contains('gallery__img')) {
+    const img = e.target;
+    e.preventDefault();
+    largeImg.src = img.parentNode.href;
+  }
+});
