73c09a8f80a260018c564dc26b9c38f2cf473f4f
Anna Kazantseva <annetastara@gmail.com>
2019-10-21 17:43:15 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,8 @@
 'use strict';
 const mainContainer = document.getElementById('thumbs');
-// eslint-disable-next-line no-shadow
-mainContainer.addEventListener('click', event => {
-  if (!event.target.closest('a')) {
-    return;
-  }
-  event.preventDefault();
-  const largeImage = document.getElementById('largeImg');
-  largeImage.src = event.target.closest('a').href;
+const largeImage = document.getElementById('largeImg');
+mainContainer.addEventListener('click', (eventTarget) => {
+  eventTarget.preventDefault();
+  largeImage.src = eventTarget.target.closest('a').href;
+  largeImage.alt = eventTarget.target.closest('a').title;
 });
