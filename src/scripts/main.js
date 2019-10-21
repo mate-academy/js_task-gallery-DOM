3cc44f9fd8c4977c169211041ffc4d1b0b823cba
Anna Kazantseva <annetastara@gmail.com>
2019-10-21 17:50:38 +0300

    Fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,8 @@
 'use strict';
 const mainContainer = document.getElementById('thumbs');
 const largeImage = document.getElementById('largeImg');
-mainContainer.addEventListener('click', (eventTarget) => {
-  eventTarget.preventDefault();
-  largeImage.src = eventTarget.target.closest('a').href;
-  largeImage.alt = eventTarget.target.closest('a').title;
+mainContainer.addEventListener('click', (eventTarg) => {
+  eventTarg.preventDefault();
+  largeImage.src = eventTarg.target.closest('a').href;
+  largeImage.alt = eventTarg.target.closest('a').title;
 });
