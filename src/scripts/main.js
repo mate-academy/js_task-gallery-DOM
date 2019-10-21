d4bc44c3c0b3488a8c10bb2f3a7660cf24417359
VadimK <djvadya16@gmail.com>
2019-10-21 15:16:51 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,12 @@ const largeImg = document.getElementById('largeImg');
 
 slideBlock.addEventListener('click', (e) => {
   e.preventDefault();
+  const largePhoto = largeImg.src;
+  const largeAlt = largeImg.alt;
   if (e.target.closest('li')) {
     largeImg.src = e.target.src;
     largeImg.alt = e.target.alt;
+    e.target.src = largePhoto;
+    e.target.alt = largeAlt;
   }
 });
