badb92d638ac500d25f9f511eeea2b4778be6234
Leon <FeelineKing@gmail.com>
2019-10-22 01:51:25 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('hello mate academy!');
+const thumbs = document.querySelector('#thumbs');
+const replaceableImage = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', function(switcher) {
+  switcher.preventDefault();
+
+  replaceableImage.src = switcher.target.closest('a').href;
+  replaceableImage.alt = switcher.target.closest('a').title;
+});
