07b13f08f4089a09f02529c1f1d19caacf55a9fb
Karpyshyn Roman <karpyshyn.roman@gmail.com>
2019-10-21 15:40:01 +0300

    s

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('hello mate academy!');
+const largeImg = document.querySelector('.gallery__large-img');
+const list = document.querySelector('#thumbs');
+
+list.addEventListener('click', (myEvent) => {
+  myEvent.preventDefault();
+  const clicked = myEvent.target.closest('.list-item__link');
+  largeImg.src = clicked.href;
+  largeImg.alt = clicked.title;
+});
