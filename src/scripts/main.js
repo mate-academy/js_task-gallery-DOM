52246bb7954e9c2acebceb3bce749b8b701dc92c
Ihor Stavratii <igor.stavratiy@gmail.com>
2019-11-28 17:59:04 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,7 @@ const imageChanger = (ev) => {
   if (ev.target.className.includes('gallery__thumb')) {
     ev.preventDefault();
     largeImg.src = ev.target.parentElement.href;
+    largeImg.alt = ev.target.alt;
   }
 };
 
