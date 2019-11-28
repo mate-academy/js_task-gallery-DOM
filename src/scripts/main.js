cb6704647ffc7b2dcfcaa9b04c94fc32dc7bf375
Innocent1312 <anthonydev33@gmail.com>
2019-11-28 12:02:14 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('hello mate academy!');
+const list = document.querySelector('#thumbs');
+const bigPicture = document.querySelector('#largeImg');
+
+list.addEventListener('click', choose, false);
+
+function choose(occurrence) {
+  event.preventDefault();
+
+  const target = occurrence.target;
+
+  if (target.tagName !== 'IMG') {
+    return;
+  }
+
+  bigPicture.src = target.parentNode.href;
+  bigPicture.alt = target.alt;
+}
