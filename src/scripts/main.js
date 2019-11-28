008f58c1440ed2de039273fe8cb97fb6a7fbf1a8
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2019-11-28 16:52:40 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('hello mate academy!');
+const image = document.querySelector('#largeImg');
+const list = document.querySelector('#thumbs');
+
+list.addEventListener('click', changeImage);
+
+function changeImage(e) {
+  if (e.target.className.includes('gallery__thumb')) {
+    e.preventDefault();
+  }
+
+  image.src = e.target.parentNode.href;
+  image.alt = e.target.list;
+}
