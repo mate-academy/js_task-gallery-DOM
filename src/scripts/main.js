13ebd424da0c1b08daf23a2f5bf57c6d3376aabc
Anna Gerdii <annagerdiy@gmail.com>
2019-11-28 14:39:41 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('hello mate academy!');
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbs.onclick = (action) => {
+  const thumbnail = action.target.closest('.list-item__link');
+
+  if (thumbnail) {
+    action.preventDefault();
+    largeImg.src = thumbnail.href;
+    largeImg.alt = thumbnail.title;
+  }
+};
