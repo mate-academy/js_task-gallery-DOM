d112bd84470a37ea1efdb10bde60b13c40a7a3da
YuliiaChupryna <julia.chuprina2907@gmail.com>
2019-11-28 21:10:33 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('hello mate academy!');
+const largeImg = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.onclick = function(ev) {
+  const thumbnail = ev.target.closest('a');
+
+  if (!thumbnail) {
+    return;
+  }
+  ev.preventDefault();
+  largeImg.src = thumbnail.href;
+  largeImg.alt = thumbnail.title;
+};
