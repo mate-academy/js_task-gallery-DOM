572f70374e37b64a20883b2f23b9aa6726a2f977
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-11-28 22:16:21 +0200

    Aidinov Alexii

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('hello mate academy!');
+const largeImg = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+const chooseThumbnail = (click) => {
+  const chosen = click.target.closest('.list-item__link');
+
+  if (!chosen || !thumbs.contains(chosen)) {
+    return;
+  }
+
+  click.preventDefault();
+  largeImg.src = chosen.href;
+};
+
+thumbs.addEventListener('click', chooseThumbnail);
