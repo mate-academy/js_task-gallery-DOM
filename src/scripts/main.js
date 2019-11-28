2bd2df5977f02543d4b84366bbe85d7871b473a3
Alexandra Boyko <Alexboyko04@gmail.com>
2019-11-28 15:47:32 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('hello mate academy!');
+const list = document.querySelector('.gallery__list');
+const mainImage = document.querySelector('.gallery__large-img');
+
+list.onclick = function(action) {
+  const thumbnail = action.target.closest('a');
+
+  if (!thumbnail) {
+    return;
+  }
+  mainImage.src = thumbnail.href;
+  mainImage.alt = thumbnail.title;
+  event.preventDefault();
+};
