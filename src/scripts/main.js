cc2aec4a24748f169d6da833f9c630da4a416817
Alex Hodovas <o.hodovas@gmail.com>
2019-11-30 17:48:22 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('hello mate academy!');
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', function(e) {
+  const thumbnail = event.target.closest('a');
+
+  if (!thumbnail) {
+    return;
+  }
+  showThumbnail(thumbnail.href, thumbnail.title);
+  event.preventDefault();
+});
+
+function showThumbnail(href, title) {
+  largeImg.src = href;
+  largeImg.alt = title;
+}
