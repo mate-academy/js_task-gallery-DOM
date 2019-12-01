8f0555f0b8b6680f91410262a6dfb8d8ab594913
PavloPantus <mr.pantus@gmail.com>
2019-12-02 00:43:27 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('hello mate academy!');
+const galleryList = document.querySelector('.gallery')
+  .querySelector('.gallery__list');
+
+const galleryLargeImg = document.querySelector('.gallery')
+  .querySelector('.gallery__large-img');
+
+galleryList.addEventListener('click', (myevent) => {
+  myevent.preventDefault();
+
+  if (event.target.matches('.gallery__thumb')) {
+    galleryLargeImg.setAttribute('src', `${myevent.target.src
+      .split('-thumb.jpeg').join('.png')}`);
+  }
+});
