80158bb11bf27680ef56919eac0a451a60511ec5
Liliya Taran <liliyataran2017@gmail.com>
2019-12-02 13:14:36 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('hello mate academy!');
+const list = document.querySelector('.gallery__list');
+const mainImage = document.querySelector('.gallery__large-img');
+
+list.onclick = el => {
+  const thumbnail = el.target.closest('a');
+
+  if (!thumbnail) {
+    return;
+  }
+  el.preventDefault();
+  mainImage.src = thumbnail.href;
+  mainImage.alt = thumbnail.title;
+};
