14bcfdf6809305edaeee45f110a1abb5ab24a5e8
Inna <inna.tereshchenko1@gmail.com>
2019-12-02 15:29:55 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log('hello mate academy!');
+const mainImage = document.querySelector('.gallery__large-img');
+
+document.body.querySelector('ul').addEventListener('click', changeOnClick);
+
+function changeOnClick(ev) {
+  ev.preventDefault();
+  mainImage.src = ev.target.parentElement.href;
+}
