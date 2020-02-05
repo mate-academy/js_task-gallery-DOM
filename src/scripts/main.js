b1cdcfa0b913d615ed53de5fbb5e2559ccad831a
Vitalii <vitaliybondarenko1982@gmail.com>
2020-02-05 18:58:09 +0200

    edit readme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,13 @@
 const galleryList = document.querySelector('.gallery__list');
 const bigImage = document.querySelector('.gallery__large-img');
 
-galleryList.addEventListener('click', (evt) => {
+function changeImage(evt) {
   evt.preventDefault();
 
   const item = evt.target.closest('.list-item__link');
   const imgValue = item.getAttribute('href');
 
   bigImage.setAttribute('src', imgValue);
-});
+}
+
+galleryList.addEventListener('click', changeImage);
