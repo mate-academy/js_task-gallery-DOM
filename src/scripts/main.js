ef32b8e1eaaf62f0a3259e63cc6ce12f28ae80f5
Senya Volkov <formobileprima@gmail.com>
2020-02-05 14:44:32 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('hello mate academy!');
+const bigImg = document.querySelector('.gallery__large-img');
+
+function replaceImg(e) {
+  if (e.target.className.includes('gallery__thumb')
+    || e.target.className.includes('list-item__link')) {
+    e.preventDefault();
+
+    const currentSource = e.target.getAttribute('src')
+      || e.target.getAttribute('href');
+
+    bigImg.setAttribute('src', currentSource);
+  }
+}
+
+document.body.addEventListener('click', replaceImg);
