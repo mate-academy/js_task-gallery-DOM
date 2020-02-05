e5d9de2a3b765d3ecc4d59c424c8ead9ec99df36
Vitalii <vitaliybondarenko1982@gmail.com>
2020-02-05 18:54:29 +0200

    on review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('hello mate academy!');
+const galleryList = document.querySelector('.gallery__list');
+const bigImage = document.querySelector('.gallery__large-img');
+
+galleryList.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  const item = evt.target.closest('.list-item__link');
+  const imgValue = item.getAttribute('href');
+
+  bigImage.setAttribute('src', imgValue);
+});
