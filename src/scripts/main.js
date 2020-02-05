b43c9e906a5fd37e9cc82fe1fcfe4bc3059d24c7
Yuliya312 <yu312liya@gmail.com>
2020-02-05 20:37:46 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('hello mate academy!');
+const galery = [...document.querySelectorAll('.gallery__thumb')];
+const bigImg = document.querySelector('#largeImg');
+
+galery.forEach(picture => {
+  picture.addEventListener('click', function(elem) {
+    elem.preventDefault();
+
+    const pictureSrc = picture.getAttribute('src', picture);
+
+    bigImg.setAttribute('src', pictureSrc);
+  });
+});
