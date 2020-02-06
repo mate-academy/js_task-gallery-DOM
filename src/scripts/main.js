9efc3c0fc1d9419661c9a871ecfafbfa112ff130
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-02-06 14:16:08 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('hello mate academy!');
+const largeImg = document.querySelector('.gallery__large-img');
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', () => {
+  event.preventDefault();
+
+  const smallImage = event.target.closest('.list-item__link');
+
+  largeImg.src = smallImage.href;
+});
