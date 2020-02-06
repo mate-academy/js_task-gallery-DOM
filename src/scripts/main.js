4565fc12358d186a078752f41de8d253773c4d77
Arimazz <bodyaprymyak@icloud.com>
2020-02-06 20:17:52 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('hello mate academy!');
+let mainImage = document.querySelector('#largeImg');
+const links = document.querySelectorAll('.list-item__link');
+
+links.forEach(link => {
+  link.addEventListener('click', (elem) => {
+    elem.preventDefault();
+
+    const imageToChange = link.attributes.href.value;
+
+    mainImage.outerHTML = `<img id="largeImg"
+    src="${imageToChange}"
+    alt="main image"
+    class="gallery__large-img gallery__img">`;
+
+    mainImage = document.querySelector('#largeImg');
+  });
+});
