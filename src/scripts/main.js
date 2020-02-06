f0d9eaaff27723c334b20400be800c5c3339e2e0
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-02-06 16:18:58 +0200

    gallery complited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('hello mate academy!');
+const largeImg = document.querySelector('.gallery__large-img');
+const imgsList = document.querySelector('.gallery__list');
+
+function clickHandler() {
+  event.preventDefault();
+
+  if (event.target.tagName === 'IMG') {
+    largeImg.src = event.target.src.replace('-thumb.jpeg', '.png');
+  }
+}
+
+imgsList.addEventListener('click', clickHandler);
