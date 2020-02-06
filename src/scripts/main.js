7a7d689e80575939dbbb3a59f6f23706100252e3
Stanislav Mochyr <delenjer@gmail.com>
2020-02-06 09:09:57 +0200

    js_gallery-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('hello mate academy!');
+const largeImg = document.querySelector('#largeImg');
+const linkCollections = document.querySelectorAll('.list-item__link');
+
+for (const item of linkCollections) {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    largeImg.src = item.href;
+  });
+}
