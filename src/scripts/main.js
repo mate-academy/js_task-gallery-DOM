9ec70b3b6c4fddccf3c67ab3b30a7a3d21992eab
Arimazz <bodyaprymyak@icloud.com>
2020-02-07 15:47:30 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,12 @@
 'use strict';
 
-let mainImage = document.querySelector('#largeImg');
+const mainImage = document.querySelector('#largeImg');
 const links = document.querySelectorAll('.list-item__link');
 
 links.forEach(link => {
   link.addEventListener('click', (elem) => {
     elem.preventDefault();
 
-    const imageToChange = link.attributes.href.value;
-
-    mainImage.outerHTML = `<img id="largeImg"
-    src="${imageToChange}"
-    alt="main image"
-    class="gallery__large-img gallery__img">`;
-
-    mainImage = document.querySelector('#largeImg');
+    mainImage.setAttribute('src', elem.currentTarget.getAttribute('href'));
   });
 });
