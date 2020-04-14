b5f937e22ede213b78daba6f6920aed720606bff
WincherZ <wincher2016@gmail.com>
2020-04-14 15:48:09 +0300

    some changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('hello mate academy!');
+const galleryImages = () => {
+  const gallery = document.querySelector('.gallery');
+
+  gallery.addEventListener('click', e => {
+    const largeImg = document.querySelector('.gallery__large-img');
+    const targetClass = e.target.classList;
+
+    if (targetClass.contains('gallery__img')) {
+      e.preventDefault();
+
+      let item = e.target.src;
+
+      item = item.replace('-thumb.jpeg', '.png');
+      largeImg.src = item;
+    };
+
+    if (targetClass.contains('list-item__link')) {
+      e.preventDefault();
+
+      let item = e.target.children[0].src;
+
+      item = item.replace('-thumb.jpeg', '.png');
+      largeImg.src = item;
+    }
+  });
+};
+
+galleryImages();
