a892eeadc034c5648b2ea657fb3642a8475d1108
Oleksii Mykhailov <alem.coding@gmail.com>
2020-04-14 19:00:49 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('hello mate academy!');
+const gallery = document.querySelector('.gallery');
+const largeImage = gallery.querySelector('#largeImg');
+const galleryList = gallery.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.gallery__item');
+
+  if (!item || !galleryList.contains(item)) {
+    return;
+  }
+
+  const imageSrc = (e.target.src).replace('-thumb', '')
+    .replace('.jpeg', '.png');
+
+  largeImage.src = imageSrc;
+});
