2107ee0210e2d2c169630f9c9357cd82aba51c89
AlenaLoik <CrazyBeeLviv@gmail.com>
2020-04-15 21:17:13 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('hello mate academy!');
+const largeImg = document.querySelector('#largeImg');
+const smallImg = document.querySelectorAll('.gallery__thumb');
+const imgLink = document.querySelectorAll('.list-item__link');
+
+function removeLink(item) {
+  let i = 0;
+
+  do {
+    item[i].href = '#';
+    i++;
+  } while (i < item.length);
+}
+
+removeLink(imgLink);
+
+function addOnclick() {
+  let i = 0;
+
+  do {
+    smallImg[i].onclick = function() {
+      largeImg.src = this.src;
+    };
+
+    i++;
+  } while (i < smallImg.length);
+}
+
+addOnclick(smallImg);
