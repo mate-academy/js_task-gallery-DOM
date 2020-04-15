bd233cf0e09b33cb00ca6abc765324fa4789e033
Alex Machula <alexmachulamail@gmail.com>
2020-04-15 21:03:58 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('hello mate academy!');
+const largeImg = document.querySelector('#largeImg');
+const smallImgs = document.querySelector('#thumbs');
+
+smallImgs.addEventListener('click', (eve) => {
+  const item = eve.target.closest('.list-item__link');
+
+  if (!item) {
+    return;
+  }
+
+  eve.preventDefault();
+  largeImg.src = item.href;
+});
