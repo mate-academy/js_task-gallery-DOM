171e4abf9649cf8a115dd9afd8775d42b71eb568
ShapovalDenys <denys.igorevich@gmail.com>
2020-04-15 16:13:42 +0300

    solution v0.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('hello mate academy!');
+const ul = document.querySelector('.gallery__list');
+const main = document.querySelector('.gallery__large-img');
+
+ul.addEventListener('click', changeLink);
+
+function changeLink() {
+  event.preventDefault();
+
+  const link = event.target.closest('a');
+
+  main.src = link;
+}
