5ee7ca35082f0bc4d627558acd04d3987450a33c
kate <kate@test.com>
2020-04-16 00:15:14 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('hello mate academy!');
+const galeryList = document.querySelector('.gallery__list');
+
+galeryList.onclick = function(arg) {
+  const thumbnail = arg.target.closest('a');
+
+  if (!thumbnail) {
+    return;
+  }
+  showThumbnail(thumbnail.href, thumbnail.title);
+  arg.preventDefault();
+};
+
+function showThumbnail(href, title) {
+  const largeImg = document.getElementById('largeImg');
+
+  largeImg.src = href;
+  largeImg.alt = title;
+}
