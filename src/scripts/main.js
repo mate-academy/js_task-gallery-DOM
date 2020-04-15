f2ea46550bd7246fe44b10e512e17c4fdf5f4cf6
Tetiana Zakharova <kazakova.tanya@gmail.com>
2020-04-15 18:31:03 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('hello mate academy!');
+const largeImg = document.querySelector('.gallery__large-img');
+const ul = document.querySelector('#thumbs');
+// const li = document.querySelector('.list-item');
+
+function mainPicture(href, title) {
+  largeImg.src = href;
+  largeImg.alt = title;
+}
+
+ul.onclick = function(eventClick) {
+  const clickedLink = eventClick.target.closest('a');
+
+  if (!clickedLink) {
+    return false;
+  }
+
+  mainPicture(clickedLink.href, clickedLink.title);
+  eventClick.preventDefault();
+};
