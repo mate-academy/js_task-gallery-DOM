5ec0a1779f22679f4352b29c01a4ed9e80ed5905
olenazubashenko <olenazubashenko@gmail.com>
2020-04-15 19:04:03 +0300

    task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('hello mate academy!');
+thumbs.addEventListener("click", event => {
+  event.preventDefault();
+  const target = event.target;
+  const link = target.closest("a");
+
+  if(link) {
+    show(link);
+  }
+})
+
+function show(link) {
+  largeImg.src = link.href;
+  largeImg.alt = link.title;
+}
