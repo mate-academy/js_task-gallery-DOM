01e829f50947c3f0a51ef88da70e4297be5058d0
Aleksandr-Tyagun <alexander.tiagun@gmail.com>
2020-04-15 21:20:41 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('hello mate academy!');
+const ul = document.querySelector('.gallery__list');
+const largeImg = document.getElementById('largeImg');
+
+function handler() {
+  event.preventDefault();
+
+  const targetHref = event.target.parentElement.href;
+
+  if (largeImg.src !== targetHref
+    && event.target.classList.contains('gallery__thumb')) {
+    largeImg.src = targetHref;
+  }
+}
+ul.addEventListener('click', handler);
