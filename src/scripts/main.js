b4b82d57dae07bbfd61b3b6b02118512586118f9
Nastia-rosy <nastiiarosy@gmail.com>
2020-04-16 16:41:24 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,1 @@
 'use strict';
-
-const mainImg = document.querySelector('#largeImg');
-
-const images = document.querySelector('#thumbs');
-
-images.addEventListener('click', (e) => {
-  e.preventDefault();
-
-  if (e.target.tagName !== 'IMG') {
-    return;
-  }
-
-  const smallImg = e.target.closest('a');
-
-  mainImg.src = smallImg.href;
-  mainImg.title = smallImg.title;
-});
