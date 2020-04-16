7b2b3b873ebafdab57428a8affe43dcbd7493599
MykolaMak <makolamak@gmail.com>
2020-04-16 21:31:41 +0300

    add js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const ima = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+ima.addEventListener('click', (evt) => {
+  if (evt.target.className.includes('gallery__thumb')) {
+    evt.preventDefault();
+    largeImg.src = evt.target.parentNode.href;
+  }
+});
