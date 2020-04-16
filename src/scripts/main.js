68c9fb24f384edb0d259ddac9da13485853bab32
Roma <roman.mykhalchuk@yahoo.com>
2020-04-16 13:47:58 +0300

    gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('hello mate academy!');
+const clickableImages = document.querySelectorAll('.gallery__thumb');
+const galleryLargeImage = document.querySelector('.gallery__img');
+
+for (const thumbImage of clickableImages) {
+  thumbImage.addEventListener('click', handler);
+}
+
+const imageLinks = document.querySelectorAll('.list-item__link');
+
+for (const link of imageLinks) {
+  link.addEventListener('click', (e) => e.preventDefault());
+}
+
+function handler(e) {
+  galleryLargeImage.src = e.target.src.replace('-thumb.jpeg', '.png');
+}
