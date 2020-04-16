b840b5ead32035562e2113be592cb80e57a68d85
kate <kate@test.com>
2020-04-16 14:36:29 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,21 @@
+
 'use strict';
 
 const galeryList = document.querySelector('.gallery__list');
+const largeImg = document.getElementById('largeImg');
 
-galeryList.onclick = function(arg) {
-  const thumbnail = arg.target.closest('a');
+galeryList.onclick = function(e) {
+  event.preventDefault();
+
+  const thumbnail = event.target.closest('a');
 
   if (!thumbnail) {
     return;
   }
   showThumbnail(thumbnail.href, thumbnail.title);
-  arg.preventDefault();
 };
 
 function showThumbnail(href, title) {
-  const largeImg = document.getElementById('largeImg');
-
   largeImg.src = href;
   largeImg.alt = title;
 }
