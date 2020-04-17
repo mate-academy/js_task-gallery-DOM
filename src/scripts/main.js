28d0eeaebacbbc47ff37c6f164bb1c14ec185735
euxos <eugen.ostrozhinsky@gmail.com>
2020-04-17 09:31:26 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('hello mate academy!');
+const galleryList = document.querySelector('.gallery__list');
+
+const hander = callback => {
+  callback.preventDefault();
+
+  const picture = callback.target.closest('.list-item__link');
+  const largeImg = document.querySelector('#largeImg');
+
+  largeImg.src = picture.href;
+};
+
+galleryList.addEventListener('click', hander);
