304b3ba54254236b8fcc25b654b9441647b93db9
euxos <eugen.ostrozhinsky@gmail.com>
2020-04-17 10:37:28 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,16 @@
 'use strict';
 
 const galleryList = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('#largeImg');
 
-const hander = callback => {
-  callback.preventDefault();
+const hander = e => {
+  e.preventDefault();
 
-  const picture = callback.target.closest('.list-item__link');
-  const largeImg = document.querySelector('#largeImg');
+  const picture = e.target.closest('.list-item__link');
 
-  largeImg.src = picture.href;
+  if (picture) {
+    largeImg.src = picture.href;
+  }
 };
 
 galleryList.addEventListener('click', hander);
