b10ca4c7475e7750c0ee4ce9817b07bf866552ae
AnnieMarkina <anniemurkina@gmail.com>
2020-04-17 10:44:41 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('hello mate academy!');
+const handleMainImg = () => {
+  const gallery = document.querySelector('.gallery');
+
+  gallery.addEventListener('click', evnt => {
+    const mainImg = document.querySelector('.gallery__large-img');
+    const target = evnt.target.classList;
+
+    if (target.contains('gallery__img')) {
+      evnt.preventDefault();
+
+      let img = evnt.target.src;
+
+      img = img.replace('-thumb.jpeg', '.png');
+      mainImg.src = img;
+    };
+
+    if (target.contains('list-item__link')) {
+      evnt.preventDefault();
+
+      let img = evnt.target.children[0].src;
+
+      img = img.replace('-thumb.jpeg', '.png');
+      mainImg.src = img;
+    }
+  });
+};
+
+handleMainImg();
