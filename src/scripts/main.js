d76aac4a9549b99ba3a256836618161a155e13ff
Igor Goncharenko <qoa11a.merry@gmail.com>
2020-04-21 00:39:40 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('hello mate academy!');
+const images = document.querySelector('#thumbs');
+const largeImage = document.querySelector('#largeImg');
+
+images.addEventListener('click', (ev) => {
+  if (ev.target.className.includes('gallery__thumb')) {
+    ev.preventDefault();
+    largeImage.src = ev.target.parentNode.href;
+  }
+});
