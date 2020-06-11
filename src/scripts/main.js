e7659e3ae9612afaa6dc3d0b8e379ffe9df7c76d
aleksey-10 <alexcare1337@gmail.com>
2020-06-11 12:35:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('hello mate academy!');
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  if (e.target.parentElement.classList.contains('list-item__link')) {
+    largeImg.src = e.target.src.replace('-thumb.jpeg', '.png');
+  }
+});
