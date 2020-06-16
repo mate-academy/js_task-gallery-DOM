837c02b3adebd3eea1764ee7fb52a0849936cda2
Denys <gryshko_denys@gmail.com>
2020-06-16 20:19:09 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('hello mate academy!');
+const list = document.querySelector('.gallery__list');
+const largeImage = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', changeMainPhoto);
+
+function changeMainPhoto(e) {
+  e.preventDefault();
+
+  const image = e.target;
+
+  if (e.target.tagName !== 'IMG') {
+    return;
+  }
+
+  const path = image.getAttribute('src');
+  const newPath = path.slice(0, -11) + '.png';
+
+  largeImage.setAttribute('src', newPath);
+}
