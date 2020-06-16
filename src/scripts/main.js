571b228cb0c3adbf487057c415e0887d0e63839f
Campari229 <KennyKoin@gmail.com>
2020-06-16 18:08:10 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('hello mate academy!');
+const smallImages = document.querySelectorAll('.gallery__thumb');
+const largeImg = document.querySelector('#largeImg');
+
+for (const smallImage of smallImages) {
+  smallImage.onclick = function(e) {
+    e.preventDefault();
+    largeImg.src = smallImage.src.replace('-thumb.jpeg', '.png');
+  };
+};
