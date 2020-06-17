65f0d8315545a0889fd00cab10a12d7e0b192c74
Vitalii Kuzmichov <43987305+Kussmich@users.noreply.github.com>
2020-06-17 11:37:52 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('hello mate academy!');
+const mainImage = document.querySelector('#largeImg');
+const smallImages = document.querySelector('#thumbs');
+
+smallImages.addEventListener('click', function(e) {
+  const target = e.target;
+  const imageSource = target.closest('a').getAttribute('href');
+
+  e.preventDefault();
+
+  mainImage.setAttribute('src', imageSource);
+});
