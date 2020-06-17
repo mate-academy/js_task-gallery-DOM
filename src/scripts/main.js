cf91fd82491260680446e7f6d5b075c95d26eabb
DTR <t.terry.rice@gmail.com>
2020-06-17 17:08:06 +0300

    final 1.3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-console.log('hello mate academy!');
+const galleryMain = document.querySelector('.gallery');
+const mainImg = galleryMain.querySelector('#largeImg');
+const list = galleryMain.querySelector('#thumbs');
+
+function changeImg(changeEvent) {
+  changeEvent.preventDefault();
+
+  if (changeEvent.target.tagName !== 'UL') {
+    mainImg.src = changeEvent.target.parentElement.href;
+  }
+}
+
+function overThumbs(overEvent) {
+  if (overEvent.target.tagName === 'IMG') {
+    overEvent.target.style.border = '3px solid cadetBlue';
+  }
+}
+
+function outThumbs(outEvent) {
+  if (outEvent.target.tagName === 'IMG') {
+    outEvent.target.style.border = '1px solid cadetBlue';
+  }
+}
+
+list.addEventListener('click', changeImg);
+list.addEventListener('mouseover', overThumbs);
+list.addEventListener('mouseout', outThumbs);
